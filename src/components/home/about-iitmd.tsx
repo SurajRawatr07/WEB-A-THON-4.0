6f6e74e1f969aecc15a97c4410fe2db4c8f27605
Suraj Rawat <rawatsuraj80627@gmail.com>
2026-08-25 16:25:58 +0530

    Update about-iitmd.tsx

--- src/components/home/about-iitmd.tsx
+++ src/components/home/about-iitmd.tsx
@@ -7,6 +7,7 @@ import { Share_Tech } from "next/font/google";
 import Link from "next/link";
 import { CursorContext } from "@/context/cursor-context";
 
+
 const shareTech = Share_Tech({ weight: "400", subsets: ["latin"] });
 
 const AboutIITMandi = () => {
